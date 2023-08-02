b1b6fd7581f3d09f0d1ab2cc6f538a3a4d81aeb3
YukoKono <130989029+YukoKono@users.noreply.github.com>
2023-08-02 16:28:02 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,107 @@
+# install demucs
+! pip install demucs
+
+# clone github code
+! git clone https://github.com/cedro3/demucs.git
+%cd demucs
+
+# import library
+import io
+from pathlib import Path
+import select
+from shutil import rmtree
+import subprocess as sp
+import sys
+from typing import Dict, Tuple, Optional, IO
+from google.colab import files
+import os
+import shutil
+
+# define function
+def find_files(in_path):
+    out = []
+    for file in Path(in_path).iterdir():
+        if file.suffix.lower().lstrip(".") in extensions:
+            out.append(file)
+    return out
+
+def copy_process_streams(process: sp.Popen):
+    def raw(stream: Optional[IO[bytes]]) -> IO[bytes]:
+        assert stream is not None
+        if isinstance(stream, io.BufferedIOBase):
+            stream = stream.raw
+        return stream
+
+    p_stdout, p_stderr = raw(process.stdout), raw(process.stderr)
+    stream_by_fd: Dict[int, Tuple[IO[bytes], io.StringIO, IO[str]]] = {
+        p_stdout.fileno(): (p_stdout, sys.stdout),
+        p_stderr.fileno(): (p_stderr, sys.stderr),
+    }
+    fds = list(stream_by_fd.keys())
+
+    while fds:
+        # `select` syscall will wait until one of the file descriptors has content.
+        ready, _, _ = select.select(fds, [], [])
+        for fd in ready:
+            p_stream, std = stream_by_fd[fd]
+            raw_buf = p_stream.read(2 ** 16)
+            if not raw_buf:
+                fds.remove(fd)
+                continue
+            buf = raw_buf.decode()
+            std.write(buf)
+            std.flush()
+
+def separate(inp=None, outp=None):
+    inp = inp or in_path
+    outp = outp or out_path
+    cmd = ["python3", "-m", "demucs.separate", "-o", str(outp), "-n", model]
+    if mp3:
+        cmd += ["--mp3", f"--mp3-bitrate={mp3_rate}"]
+    if float32:
+        cmd += ["--float32"]
+    if int24:
+        cmd += ["--int24"]
+    if two_stems is not None:
+        cmd += [f"--two-stems={two_stems}"]
+    files = [str(f) for f in find_files(inp)]
+    if not files:
+        print(f"No valid audio files in {in_path}")
+        return
+    print("Going to separate the files:")
+    print('\n'.join(files))
+    print("With command: ", " ".join(cmd))
+    p = sp.Popen(cmd + files, stdout=sp.PIPE, stderr=sp.PIPE)
+    copy_process_streams(p)
+    p.wait()
+    if p.returncode != 0:
+        print("Command failed, something went wrong.")
+
+def reset_folder(path):
+    if os.path.isdir(path):
+      shutil.rmtree(path)
+    os.makedirs(path,exist_ok=True)
+
+# make directory
+! mkdir target
+! mkdir separated
+
+
+
+
+# ユーザーインターフェイスの構築
+st.title(" 音源分離")
+st.write("映像あるいは音声のファイルから")
+st.image("image.png")
+
+user_input = st.text_input("アップロードしてください：", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🥺"
+        if message["role"]=="assistant":
+            speaker="🙍"
+
+        st.write(speaker + ": " + message["content"])
